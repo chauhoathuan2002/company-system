36c96f81925d0abc8c52afe459e61eb04f531b50
chauthuan2k2 <thuandc21v7n546@vlvh.ctu.edu.vn>
2026-05-01 22:23:40 +0700

    1

--- backend/data.py
+++ backend/data.py
@@ -1,10 +1,11 @@
+import os
 import psycopg2
 
 def get_conn():
     return psycopg2.connect(
-        host="db.ydkjkputdrarpdzrrhqo.supabase.co",
-        database="postgres",
-        user="postgres",
-        password="Thuan12122k2",
-        port="5432"
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
     )
--- backend/main.py
+++ backend/main.py
@@ -126,3 +126,7 @@ def change_password(data: dict):
 
 # ================= INCLUDE ROUTER =================
 app.include_router(hr_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=10000)
