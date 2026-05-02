650fac9f019d0430cb1fbe37141b04f3bca29885
chauthuan2k2 <thuandc21v7n546@vlvh.ctu.edu.vn>
2026-05-02 21:11:12 +0700

    1

--- backend/data.py
+++ backend/data.py
@@ -2,20 +2,9 @@ import os
 import psycopg2
 
 def get_conn():
-    try:
-        db_url = os.getenv("DATABASE_URL")
+    db_url = os.getenv("DATABASE_URL")
 
-        # Nếu chưa có ENV thì dùng link mặc định
-        if not db_url:
-            db_url = "postgresql://postgres:Thuan12122k2@db.ydkjkputdrarpdzrrhqo.supabase.co:6543/postgres"
+    if not db_url:
+        db_url = "postgresql://postgres:Thuan12122k2@db.ydkjkputdrarpdzrrhqo.supabase.co:5432/postgres?sslmode=require"
 
-        conn = psycopg2.connect(
-            db_url,
-            sslmode="require"
-        )
-
-        return conn
-
-    except Exception as e:
-        print("Lỗi kết nối database:", e)
-        raise e
+    return psycopg2.connect(db_url)
