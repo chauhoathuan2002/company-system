883e5bdf53ba6afe8ed90a8d471a4642197e07f0
chauthuan2k2 <thuandc21v7n546@vlvh.ctu.edu.vn>
2026-05-02 20:08:48 +0700

    1

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,15 @@ from human_resource import router as hr_router
 
 app = FastAPI()
 
-# CORS
+origins = [
+    "https://congtychauthuan.vercel.app",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
