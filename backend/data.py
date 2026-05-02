2ba9f5a6137aae2c7ec3498eace2e2d06d5a8ccb
chauthuan2k2 <thuandc21v7n546@vlvh.ctu.edu.vn>
2026-05-02 21:36:02 +0700

    Update data.py

--- backend/data.py
+++ backend/data.py
@@ -5,6 +5,6 @@ def get_conn():
     db_url = os.getenv("DATABASE_URL")
 
     if not db_url:
-        db_url = "postgresql://postgres.ydkjkputdrarpdzrrhqo:[Thuan12122k2]@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres"
+        db_url = "postgresql://postgres.ydkjkputdrarpdzrrhqo:Thuan12122k2@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres"
 
     return psycopg2.connect(db_url, sslmode="require")
